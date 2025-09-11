0f93bcd16311ce2dce7d9b7d970ef3cde22890ae
maithisuong2003 <21130516@st.hcmuaf.edu.vn>
2025-09-11 14:54:08 +0700

    handling exception with httpcode

--- backend/src/main/java/com/example/demo/exception/ErrorCode.java
+++ backend/src/main/java/com/example/demo/exception/ErrorCode.java
@@ -1,21 +1,28 @@
 package com.example.demo.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error"),
-    USER_NOT_FOUND(1001, "User not found"),
-    USER_INACTIVE(1002, "User is inactive"),
-    EMAIL_EXISTED(1003, "Email already exists"),
-    USER_EXISTED(1004, "Username already exists"),
-    ROLE_NOT_FOUND(1005, "Role not found"),
-    USER_ALREADY_INACTIVE(1006, "User is already inactive"),
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_FOUND(1001, "User not found",HttpStatus.NOT_FOUND),
+    USER_INACTIVE(1002, "User is inactive",HttpStatus.BAD_REQUEST),
+    EMAIL_EXISTED(1003, "Email already exists",HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1004, "Username already exists",HttpStatus.BAD_REQUEST),
+    ROLE_NOT_FOUND(1005, "Role not found",HttpStatus.NOT_FOUND),
+    USER_ALREADY_INACTIVE(1006, "User is already inactive",HttpStatus.BAD_REQUEST),
+    UNAUTHENTICATED(1011, "Unauthenticated",HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(107, "You do not have permission",HttpStatus.FORBIDDEN)
     ;
     private int code;
     private String message;
-    ErrorCode(int code, String message) {
+    private HttpStatusCode statusCode;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 }
